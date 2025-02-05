be60c87e7024a2da6fc71166fbf73be9280599d6
DOSSA-HASAN <hassandossa5@gmail.com>
2025-02-05 17:27:18 +0300

    navbar component

---
+++ client/secure-auth/src/components/navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import "./navbar.scss"
+import { Link } from "react-router-dom"
+
+function Navbar() {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const [isLinksOpen, setIsLinksOpen] = useState(false)
+
+    return (
+        <nav className='navigation-bar'>
+            <figure className="logo-cont">
+                <img src="/logo.png" alt="" />
+                <h1>SECURE AUTH</h1>
+            </figure>
+            <main className="links-cont">
+                <Link to={""}><p>LOGIN</p></Link>
+                <Link to={""} className='register-btn'><p>REGISTER</p></Link>
+                <figure className={ isLinksOpen ? 'account-setting display-setting' : 'account-setting'}>
+                    <i onClick={() => setIsLinksOpen(!isLinksOpen)} className="fa-solid fa-gear"></i>
+
+                    <article className={isLinksOpen ? 'settings' : 'hide-settings'}>
+                        <Link to={""}><p>RESET PASSWORD</p></Link>
+                        <Link to={""}><p>VERIFY ACCOUNT</p></Link>
+                        <Link to={""}><p>LOGOUT</p></Link>
+                        <Link to={""}><p>PROFILE</p></Link>
+                    </article>
+                </figure>
+            </main>
+            <main className="mobile-menu">
+                <div className="menu-btn" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                    <i className="fa-solid fa-bars"></i>
+                </div>
+                <article className={isMenuOpen ? "mobile-links" : "hide-mobile-links"}>
+                    <Link to={""}><p>LOGIN</p></Link>
+                    <Link to={""}><p>REGISTER</p></Link>
+                    <Link to={""}><p>RESET PASSWORD</p></Link>
+                    <Link to={""}><p>VERIFY ACCOUNT</p></Link>
+                    <Link to={""}><p>LOGOUT</p></Link>
+                    <Link to={""}><p>PROFILE</p></Link>
+                </article>
+            </main>
+        </nav>
+    )
+}
+
+export default Navbar
