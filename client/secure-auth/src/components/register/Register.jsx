289b1a623da1062c9e31025b8846dde8256c4729
DOSSA-HASAN <hassandossa5@gmail.com>
2025-02-06 11:26:24 +0300

    register component completed + responsive

--- client/secure-auth/src/components/register/Register.jsx
+++ client/secure-auth/src/components/register/Register.jsx
@@ -7,6 +7,25 @@ function Register() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [confirmPassword, setConfirmPassword] = useState("")
+    const [error, setError] = useState("")
+
+    const setPass = (e) => {
+        setPassword(e.target.value)
+        validatePasswordMatch(e.target.value, confirmPassword)
+    }
+
+    const confirmPass = (e) => {
+        setConfirmPassword(e.target.value)
+        validatePasswordMatch(password, e.target.value)
+    }
+
+    const validatePasswordMatch = (pass, confirmedPass) => {
+        if (confirmPassword && pass !== confirmedPass) {
+            setError("Passwords do not match")
+        } else {
+            setError("")
+        }
+    }
 
     return (
         <section className='register-section'>
@@ -21,12 +40,15 @@ function Register() {
                 <form>
                     <h2>Register</h2>
                     <input type="text" name="username" id="username" required placeholder='Name' onChange={(e) => setName(e.target.value)} />
-                    <input type="email" name="email" id="email" required placeholder='Email' onChange={(e) => setEmail(e.target.value)}/>
+                    <input type="email" name="email" id="email" required placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
+                    <span className='password-error'>
+                        <p>{error}</p>
+                    </span>
                     <div className="passwords">
-                        <input type="password" name="password" id="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)}/>
-                        <input type="password" name="confirm-password" id="confirm-password" placeholder='Confirm password' onChange={(e) => setConfirmPassword(e.target.value)}/>
+                        <input type="password" name="password" id="password" placeholder='Password' onChange={setPass} />
+                        <input type="password" name="confirm-password" id="confirm-password" placeholder='Confirm password' onChange={confirmPass} />
                     </div>
-                    <button>CONTINUE</button>
+                    <button type='submit' disabled={error !== ""}>CONTINUE</button>
                 </form>
             </main>
         </section>
