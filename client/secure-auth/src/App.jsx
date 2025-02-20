290d16d17c85a80fe89551d3635605b209c56773
DOSSA-HASAN <hassandossa5@gmail.com>
2025-02-20 09:37:20 +0300

    verify email component + responsive

--- client/secure-auth/src/App.jsx
+++ client/secure-auth/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/navbar/Navbar'
 import { BrowserRouter, Routes } from 'react-router-dom'
 import Register from './components/register/Register'
 import Login from './components/login/Login'
+import VerifyEmail from './components/verifyEmail/VerifyEmail'
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <BrowserRouter>
           <Navbar />
           {/* <Register /> */}
-          <Login />
+          {/* <Login /> */}
+          <VerifyEmail />
         <Routes>
         </Routes>
       </BrowserRouter>
---
+++ client/secure-auth/src/components/verifyEmail/VerifyEmail.jsx
@@ -0,0 +1,65 @@
+import React, { useRef, useState } from 'react'
+import "./verifyemail.scss"
+
+function VerifyEmail() {
+
+    const [otpValues, setOtpValues] = useState(new Array(6).fill(""))
+    const inputRefs = useRef([])
+
+    const handleOtpInput = (e, index) => {
+        const value = e.target.value
+        if (/^[0-9]$/.test(value) || value == "") {
+            const newOtpvalue = [...otpValues]
+            newOtpvalue[index] = value
+            setOtpValues(newOtpvalue)
+
+            if (value !== "" && index < 6) {
+                inputRefs.current[index + 1].focus()
+            }
+        }
+    }
+
+    const handleKeyDown = (e, index) => {
+        if (e.key === "Backspace") {
+            const newOtpValues = [...otpValues]
+            newOtpValues[index] = ""
+            setOtpValues(newOtpValues)
+
+            if (index > 0) {
+                inputRefs.current[index - 1].focus()
+            }
+        }
+    }
+
+    const handleResendOtp = () => { }
+
+    return (
+        <section className='verify-email-section'>
+            <div className="bg-deco">
+                <img src="/bg-gradient-deco.png" alt="" />
+            </div>
+            <main className='main-verify-cont'>
+                <div className="input-cont">
+                    {otpValues.map((value, index) => (
+                        <input
+                            key={index}
+                            type='number'
+                            className='box'
+                            value={value}
+                            onChange={(e) => handleOtpInput(e, index)}
+                            onKeyDown={(e) => handleKeyDown(e, index)}
+                            ref={(el) => inputRefs.current[index] = el}
+                            maxLength={1}
+                        />
+                    ))}
+                </div>
+                <div className="resend-email-cont">
+                    <p>click <span onClick={handleResendOtp}>here</span> to resend the email if you havent already received one.</p>
+                </div>
+            </main>
+
+        </section>
+    )
+}
+
+export default VerifyEmail
